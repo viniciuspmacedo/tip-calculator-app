6d50ab0517a61cd78f5296c822058710e752ddb3
viniciuspmacedo <viniciusp.macedo@gmail.com>
2024-01-21 15:39:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from "../src/components/Container/Container.js";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import './Container.css';
+
+function Container() {
+    return (
+        < div className="Container" >
+        
+        </div >
+    );
+}
+
+export default Container;
