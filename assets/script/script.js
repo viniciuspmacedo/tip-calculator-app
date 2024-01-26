99327eec4b786d2be0e61d206a5777049f416f6a
viniciuspmacedo <viniciusp.macedo@gmail.com>
2024-01-26 18:25:35 -0300

    inclusão estados ativos

--- assets/script/script.js
+++ assets/script/script.js
@@ -17,7 +17,21 @@ customTipElement.addEventListener('input', () => {
 
 billElement.addEventListener('input', showResults)
 
-peopleElement.addEventListener('input', showResults)
+peopleElement.addEventListener('error', () => {
+
+})
+
+peopleElement.addEventListener('input', () => {
+    const errorElement = document.querySelector('.error-message')
+    if (peopleElement.value === '0') {
+        errorElement.style.display = 'inline-block'
+        peopleElement.style.outlineColor = 'red'
+        showResults()
+    } else {
+        errorElement.style.display = 'none';
+        peopleElement.style.outlineColor = 'var(--strong-cyan)'
+    }
+})
 
 
 resetBtn.addEventListener('click', () => {
