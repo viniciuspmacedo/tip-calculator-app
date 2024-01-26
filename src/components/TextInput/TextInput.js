b8ef03d21d38442ca98302eb5409f328d1427a19
viniciuspmacedo <viniciusp.macedo@gmail.com>
2024-01-26 07:52:15 -0300

    delete react

--- src/components/TextInput/TextInput.js
+++
@@ -1,9 +0,0 @@
-import './TextInput.css';
-
-function TextInput(props) {
-    return(
-        <input className='text-input' type="Text" placeholder={props.placeholder}></input>
-    )
-}
-
-export default TextInput;
